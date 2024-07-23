4bc35b547c5afc7333419bd165aa11417cfd567b
huuhung7301 <48995642+huuhung7301@users.noreply.github.com>
2024-07-23 21:35:39 +1000

    Update CV

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Simon Pham graduate.pdf";
+import pdf from "../../Assets/Simon resume 24.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
